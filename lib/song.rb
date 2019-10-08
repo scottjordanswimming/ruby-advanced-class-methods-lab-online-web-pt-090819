ee67f7771c9f7176902cc910903c97db6df3ab43
Scott Jordan <scottjordanswimming@gmail.com>
2019-10-08 13:59:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -64,6 +64,7 @@ split_file = song.split(" - ")
  artist = split_file[0]
  song_name = split_file[1].split(".")[0]
   #song_name = split_file[1].gsub(".mp3", "")
+  song = self.new_by_name(song_name)
 end
 
 
